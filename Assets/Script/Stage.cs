3971a03d38224e1eaf23ed3f8750a55029a8d9ac
RizkyHendraSaputra <82431281+RizkyHendraSaputra@users.noreply.github.com>
2021-09-26 02:09:23 +0700

    Full Data Game Match Tree

---
+++ Assets/Script/Stage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage : MonoBehaviour
+{
+    public void Easy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void Medium()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+    public void Hard()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+
+}
